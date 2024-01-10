edcc600c3ec4469b3d3bd1fc072e4b1b0b87674f
Alfie stanton <alfie2.stanton@live.uwe.ac.uk>
2024-01-10 16:10:14 +0000

    slots

---
+++ GEP_Unity/Assets/Scripts/UI Scripts/InventoryDisplay.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+
+public abstract class InventoryDisplay : MonoBehaviour
+{
+    [SerializeField] MouseItemData mouseInventoryItem;
+
+    protected InventorySystem inventorySystem;
+    protected Dictionary<InventorySlot_UI, InventorySlot> slotDictionary;
+    public InventorySystem InventorySystem => inventorySystem;
+    public Dictionary<InventorySlot_UI, InventorySlot> SlotDictionary => slotDictionary;
+
+    public abstract void AssignSlot(InventorySystem invToDisplay);
+
+    protected virtual void UpdateSlot(InventorySlot updatedSlot)
+    {
+        foreach (var slot in SlotDictionary)
+        {
+            if (slot.Value == updatedSlot) // Slot value - hidden inventory slot
+            {
+                slot.Key.UpdateUISlot(updatedSlot); // slot key - the ui representation of the value.
+            }
+        }
+    }
+
+    public void SlotClicked(InventorySlot_UI clickedSlot)
+    {
+        Debug.Log("Slot Clicked");
+    }
+}
