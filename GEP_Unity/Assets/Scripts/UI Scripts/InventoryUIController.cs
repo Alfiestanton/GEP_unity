77272f4f044f56d4973aab7ec400418fafaf6f25
Alfie stanton <alfie2.stanton@live.uwe.ac.uk>
2024-01-12 19:01:26 +0000

    splitting stacks and working on chest

---
+++ GEP_Unity/Assets/Scripts/UI Scripts/InventoryUIController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryUIController : MonoBehaviour
+{
+
+    public DynamicInventoryDisplay inventoryPanel;
+
+    private void OnEnable()
+    {
+        InventoryHolder.OnDynamicInventoryDisplayRequested += DisplayInventory;
+    }
+
+    private void OnDisable()
+    {
+        InventoryHolder.OnDynamicInventoryDisplayRequested -= DisplayInventory;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void DisplayInventory(InventorySystem invToDisplay)
+    {
+        inventoryPanel.gameObject.SetActive(true);
+        inventoryPanel.RefreshDynamicInventory(invToDisplay);
+    }
+}
